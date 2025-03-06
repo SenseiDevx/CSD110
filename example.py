4fb41ab3bc816df385e43a3bd90e9de3f172d7c3
Omurbek Kurmanbekov <kurmanbekovomurbek80@gmail.com>
2025-03-06 10:55:25 -0800

    add list methods]

--- example.py
+++ example.py
@@ -1,5 +1,24 @@
-myl = [500, 400, 600][1:3]
-print(myl)
+# myl = [500, 400, 600][1:3]
+# print(myl)
+#
+# my2 = [1, 2, 3, 4]
+# print(my2)
 
-my2 = [1, 2, 3, 4]
-print(my2)
+# numbers = [1, 0, 8, 3.5, 11, 2, 5]
+# slice1 = numbers[::-3]
+# print(slice1)
+# print(numbers[-1], numbers[-2])
+
+# values = ["fifty", 3, "one", -9, 0, 0.2, "five"]
+# slice = values[:2:-3]
+# print(slice)
+
+
+
+# values = ["a", "b", "c", "d", "e"]
+
+# print(values[1:4])   # (индекс 4 не включается)
+# print(values[:3])    # (от начала до 3-го индекса)
+# print(values[2:])    # (от 2-го индекса до конца)
+# print(values[::2])   # (каждый 2-й элемент)
+# print(values[::-1])  # (разворот списка)
